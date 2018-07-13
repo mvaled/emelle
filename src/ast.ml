c6f1613d870062d198ab128447b460166742e29c
TheAspiringHacker <theaspiringhacker@protonmail.com>
2018-07-12 21:14:47 -0400

    Second commit

--- src/ast.ml
+++ src/ast.ml
@@ -1,6 +1,6 @@
 type 'a pattern = 'a * 'a pattern'
 and 'a pattern' =
-  | Con of Ident.t * 'a pattern list
+  | Con of Ident.t * Ident.t * 'a pattern list
   | Var of string
   | Wild
 
@@ -21,3 +21,10 @@ let fresh_id st =
   let ret = Ident.Autogen st.vargen in
   st.vargen <- st.vargen + 1;
   ret
+
+let rec pattern_of_ast_pattern (_, node) =
+  match node with
+  | Con(typename, con, pats) ->
+     Pattern.Con(typename, con, List.map pattern_of_ast_pattern pats)
+  | Var _ -> Pattern.Wild
+  | Wild -> Pattern.Wild
--- src/type.ml
+++ src/type.ml
@@ -2,6 +2,11 @@ type prim =
   | Int
   | Float
 
+type unassigned_var = {
+    id : int;
+    mutable level : int;
+  }
+
 type t =
   | App of t * t
   | Nominal of Ident.t
@@ -9,14 +14,23 @@ type t =
   | Var of var ref
 
 and var =
-  | Unassigned of int ref
+  | Unassigned of unassigned_var
   | Assigned of t
 
 type algebraic = (t array * int) Ident.Tbl.t
 
 type def = Alias of t | Algebraic of algebraic
 
-let rec occurs (uvar : int ref) = function
+type checker = {
+    mutable vargen : int
+  }
+
+let fresh_utvar checker level =
+  let old = checker.vargen in
+  checker.vargen <- old + 1;
+  { id = old; level = level }
+
+let rec occurs (uvar : unassigned_var) = function
   | App(tcon, targ) -> occurs uvar tcon && occurs uvar targ
   | Nominal _ -> false
   | Prim _ -> false
@@ -24,12 +38,12 @@ let rec occurs (uvar : int ref) = function
      match !cell with
      | Assigned ty -> occurs uvar ty
      | Unassigned u ->
-        if u == uvar then (* Check physical equality *)
+        if u.id = uvar.id then
           true
         else (
           (* Adjust levels if necessary *)
-          if !u > !uvar then (
-            u := !uvar
+          if u.level > uvar.level then (
+            u.level <- uvar.level
           );
           false
         )
@@ -45,12 +59,12 @@ let rec unify errs lhs rhs =
   | Prim lprim     , Prim rprim   when lprim = rprim -> ()
   | ((Var v, ty)   | (ty, Var v)) as pair ->
      begin match !v with
-     | Unassigned ref ->
+     | Unassigned uvar ->
         begin match ty with
         (* A variable occurring in itself is not an error *)
-        | Var {contents = Unassigned ref2} when ref == ref2 -> ()
+        | Var {contents = Unassigned uvar2} when uvar.id = uvar2.id -> ()
         | _ ->
-           if occurs ref ty then
+           if occurs uvar ty then
              Queue.add pair errs
            else
              ()
@@ -58,3 +72,25 @@ let rec unify errs lhs rhs =
      | Assigned t -> unify errs t ty
      end
   | pair -> Queue.add pair errs
+
+(** Instantiate a type scheme by replacing type variables whose levels are
+    greater than or equal to target_level with type variables of level
+    new_level *)
+let inst checker target_level new_level =
+  let map = Hashtbl.create 11 in
+  let rec helper = function
+    | App(tcon, targ) -> App(helper tcon, helper targ)
+    | Var { contents = Assigned ty } -> helper ty
+    | (Var { contents = Unassigned uvar }) as var ->
+       if uvar.level < target_level then
+         var
+       else
+         begin match Hashtbl.find_opt map uvar.id with
+         | Some uvar2 -> Var (ref (Unassigned uvar2))
+         | None ->
+            let uvar2 = fresh_utvar checker new_level in
+            Hashtbl.add map uvar.id uvar2;
+            Var (ref (Unassigned uvar2))
+         end
+    | ty -> ty
+  in helper
