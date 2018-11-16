e3f5aacb1fe6a81abfe1d33c27e093097d7333c3
TheAspiringHacker <theaspiringhacker@protonmail.com>
2018-11-15 20:55:51 -0500

    Improve web interface with CSS and error messages

--- try/main.ml
+++ try/main.ml
@@ -11,7 +11,11 @@ let () =
           | None -> assert false
           | Some textarea -> textarea
         in
+        let console = Dom_html.getElementById "console" in
         let button = Dom_html.getElementById "compile" in
+        let set_console_text str =
+          console##.textContent := Js.some (Js.string str)
+        in
         button##.onclick :=
           Dom.handler (fun _ ->
               begin match
@@ -21,9 +25,19 @@ let () =
                 |> Pipeline.compile (Hashtbl.create (module String)) "main"
               with
               | Ok _ ->
-                 Caml.print_endline "OK!"
-              | Error _ ->
-                 Caml.print_endline "ERROR!"
+                 Caml.print_endline "OK!";
+                 set_console_text ""
+              | Error errs ->
+                 Caml.print_endline "ERROR!";
+                 let pp = Prettyprint.create () in
+                 Sequence.iter ~f:(Prettyprint.print_error pp) errs;
+                 set_console_text (Prettyprint.to_string pp)
+              | exception (Parser.Error | Lexer.Error _) ->
+                 Caml.print_endline "ERROR";
+                 set_console_text "Syntax error"
+              | exception _ ->
+                 Caml.print_endline "ERROR";
+                 set_console_text "Unknown error"
               end;
               Js._true
             );
