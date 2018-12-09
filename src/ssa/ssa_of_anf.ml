6161e9828ab4ad58694fd6ac8915148a7cc14b3b
TheAspiringHacker <theaspiringhacker@protonmail.com>
2018-12-09 13:50:43 -0500

    Replace explicit state passing with mutation in ssa_of_anf.ml

--- src/ssa/ssa_of_anf.ml
+++ src/ssa/ssa_of_anf.ml
@@ -4,10 +4,10 @@
 open Base
 
 type t = {
-    package : Ssa.package;
-    blocks : (int, Ssa.basic_block, Int.comparator_witness) Map.t;
-    next_label : int;
-    next_proc : int;
+    package : Ssa.package ref;
+    mutable blocks : (int, Ssa.basic_block, Int.comparator_witness) Map.t;
+    label_gen : int ref;
+    proc_gen : int ref;
     instrs : Ssa.instr Queue.t;
     curr_cont : Ssa.cont;
     cont : Ssa.cont;
@@ -17,12 +17,13 @@ type t = {
     index of the next basic block and returns [cont]'s result *)
 let fresh_block ctx ~cont =
   let open Result.Monad_infix in
-  let idx = ctx.next_label in
+  let idx = !(ctx.label_gen) in
+  ctx.label_gen := idx + 1;
   let instrs = Queue.create () in
-  cont { ctx with next_label = idx + 1 } instrs idx >>| fun (ctx, ret, tail) ->
+  cont instrs idx >>| fun (ret, tail) ->
   let block = { Ssa.instrs; tail } in
-  let blocks = Map.set ctx.blocks ~key:idx ~data:block in
-  ({ ctx with blocks }, ret)
+  ctx.blocks <- Map.set ctx.blocks ~key:idx ~data:block;
+  ret
 
 let rec compile_opcode ctx anf ~cont =
   let open Result.Monad_infix in
@@ -35,60 +36,57 @@ let rec compile_opcode ctx anf ~cont =
      cont ctx (Ssa.Call(f, arg, args))
   | Anf.Case(_scruts, tree, join_points) ->
      (* The basic block for the case expr continuation *)
-     fresh_block ctx ~cont:(fun ctx cont_instrs cont_idx ->
+     fresh_block ctx ~cont:(fun cont_instrs cont_idx ->
          List.fold_right ~f:(fun (_, instr) acc ->
-             acc >>= fun (ctx, list) ->
+             acc >>= fun list ->
              (* The basic block for the join point *)
-             fresh_block ctx ~cont:(fun ctx branch_instrs branch_idx ->
+             fresh_block ctx ~cont:(fun branch_instrs branch_idx ->
                  compile_instr
                    { ctx with
                      instrs = branch_instrs
                    ; curr_cont = Ssa.Block branch_idx
                    ; cont = Ssa.Block cont_idx }
                    instr
-                 >>| fun (ctx, tail, phi_elem) ->
-                 (ctx, (branch_idx, phi_elem), tail)
-               ) >>| fun (ctx, branch) ->
-             (ctx, branch::list)
-           ) ~init:(Ok (ctx, [])) join_points
-         >>= fun (ctx, branches) ->
+                 >>| fun (tail, phi_elem) ->
+                 ((branch_idx, phi_elem), tail)
+               ) >>| fun branch ->
+             branch::list
+           ) ~init:(Ok []) join_points
+         >>= fun branches ->
          compile_decision_tree ctx cont_idx (Array.of_list branches) tree
-         >>= fun (ctx, cont_from_decision_tree) ->
+         >>= fun cont_from_decision_tree ->
          (* Compile the rest of the ANF in the continuation block *)
          cont
            { ctx with
              instrs = cont_instrs
            ; curr_cont = Ssa.Block cont_idx }
            (Ssa.Phi branches)
-         >>| fun (ctx, tail, result) (* Tail of continuation block *) ->
+         >>| fun (tail, result) (* Tail of continuation block *) ->
          (* Cont for origin block, cont for continuation block *)
-         (ctx, (cont_from_decision_tree, result), tail)
-       ) >>| fun (ctx, (tail, result)) ->
-     (ctx, tail, result)
+         ((cont_from_decision_tree, result), tail)
+       )
   | Anf.Fun proc ->
      let env = proc.Anf.env in
-     compile_proc ctx proc >>= fun (ctx, proc) ->
-     let idx = ctx.next_proc in
-     let procs = Map.set ctx.package.Ssa.procs ~key:idx ~data:proc in
-     cont
-       { ctx with
-         next_proc = idx + 1
-       ; package = { procs } }
-       (Ssa.Fun(idx, env))
+     compile_proc ctx proc >>= fun proc ->
+     let idx = !(ctx.proc_gen) in
+     ctx.proc_gen := idx + 1;
+     let procs = Map.set !(ctx.package).Ssa.procs ~key:idx ~data:proc in
+     ctx.package := { procs };
+     cont ctx (Ssa.Fun(idx, env))
   | Anf.Load o -> cont ctx (Ssa.Load o)
   | Anf.Prim p -> cont ctx (Ssa.Prim p)
   | Anf.Ref x -> cont ctx (Ssa.Ref x)
 
-and compile_decision_tree ctx _cont_idx branches = function
+and compile_decision_tree _ctx _cont_idx branches = function
   | Anf.Deref(_, _occ, _tree) ->
      failwith ""
   | Anf.Fail ->
      failwith ""
   | Anf.Leaf(_id, _occs, idx) ->
      let branch_idx, _ = branches.(idx) in
-     Ok (ctx, Ssa.Block branch_idx)
+     Ok (Ssa.Block branch_idx)
   | Anf.Switch(_, _occ, _trees, _tree) ->
-     Ok (ctx, Ssa.Switch)
+     Ok (Ssa.Switch)
 
 and compile_instr ctx = function
   | Anf.Let(reg, op, next) ->
@@ -112,7 +110,7 @@ and compile_instr ctx = function
          compile_instr ctx next
        )
   | Anf.Break operand ->
-     Ok (ctx, ctx.cont, operand)
+     Ok (ctx.cont, operand)
 
 and compile_proc ctx proc =
   let open Result.Monad_infix in
@@ -122,14 +120,11 @@ and compile_proc ctx proc =
     { ctx with
       blocks
     ; instrs
-    ; next_label = 0
+    ; label_gen = ref 0
     ; curr_cont = Ssa.Entry
     ; cont = Ssa.Return }
   in
-  compile_instr state proc.Anf.body >>| fun (state, tail, return) ->
-  ( { ctx with
-      package = state.package
-    ; next_proc = state.next_proc }
-  , { Ssa.blocks
-    ; entry = { instrs; tail }
-    ; return = return } )
+  compile_instr state proc.Anf.body >>| fun (tail, return) ->
+  { Ssa.blocks
+  ; entry = { instrs; tail }
+  ; return = return }
